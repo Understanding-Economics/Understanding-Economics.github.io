21255a75f02c6204b58812b14774222fa1ddbbbc
Kevin Bi <keveloper@gmail.com>
2020-01-02 16:49:54 -0600

    I think setup is finally finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {groups, surveys} from './config/fields.json'
+import NavigationBar from './components/NavigationBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Under construction
+        <NavigationBar surveys={surveys}/>
       </div>
     );
   }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NavigationBar extends Component { 
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
