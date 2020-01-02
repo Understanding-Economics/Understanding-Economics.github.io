c8b1d7ee297a491c50792bb448faf01054840ad0
Kevin Bi <keveloper@gmail.com>
2020-01-02 15:48:00 -0600

    Updates

---
+++ precache-manifest.412d1d95d153b528c1c1ab53b55e268b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68efc48dbff5d233185665277b37501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ddc8e43340726f9f24",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "57e80dbe720f63897170",
+    "url": "/static/css/main.5ecac597.chunk.css"
+  },
+  {
+    "revision": "d3ddc8e43340726f9f24",
+    "url": "/static/js/2.a6f70c6d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6f70c6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57e80dbe720f63897170",
+    "url": "/static/js/main.1b78db2a.chunk.js"
+  },
+  {
+    "revision": "506d1216269fa0341499",
+    "url": "/static/js/runtime-main.b7447a5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
