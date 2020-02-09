ccf2adb14e77b4e09293276392793da82d8873ad
Kevin Bi <keveloper@gmail.com>
2020-02-08 19:51:38 -0500

    Create Colors.js

---
+++ src/Colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    Histogram : ["#00e699"],
+    Categorical : ["#00e699", "#008ae6", "#aa80ff", "#ff80ff", "#ff6666", "#ff9933"],
+    Gradient3 : ["#00e699", "#008ae6", "#aa80ff"], 
+    Gradient4 : ["#00e699", "#008ae6", "#aa80ff", "#ff6666"], 
+    Gradient5 : ["#00e699", "#008ae6", "#aa80ff", "#ff80ff", "#ff6666"]
+}
+
+export default Colors
