3cf2577306e30bc63b73eac1ab51eeb15533b940
Kevin Bi <keveloper@gmail.com>
2020-02-10 14:37:50 -0500

    Updates

---
+++ precache-manifest.3d3aaa99f7a8948f680d787706aa6745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d720a63fe6d06b4771cafa854890f8bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4726ce62de042e0193c",
+    "url": "/static/css/main.89c0aed7.chunk.css"
+  },
+  {
+    "revision": "6a628b111fda7abdef1e",
+    "url": "/static/js/2.d367d73e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d367d73e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4726ce62de042e0193c",
+    "url": "/static/js/main.c46e876a.chunk.js"
+  },
+  {
+    "revision": "506d1216269fa0341499",
+    "url": "/static/js/runtime-main.b7447a5d.js"
+  }
+]);
