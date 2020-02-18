4377d9f7f68fd4f05384a742e0f7507242c1f79d
Kevin Bi <keveloper@gmail.com>
2020-02-18 15:02:00 -0500

    Add proportions calculator

--- src/Utils.js
+++ src/Utils.js
@@ -3,6 +3,17 @@ const Utils = {
         let vals = data.map(x => x[key]);
         var uniqueVals = vals.filter((v, i, a) => a.indexOf(v) === i);
         return uniqueVals.filter(x => x.trim().length > 0).sort(sorter);
+    }, 
+
+    // For each group, gets the proportion of each response
+    getProportions : function(data, groupKey, groupVal, responseKey) {
+        let responseVals = getUniqueDictVals(data, responseKey, null);
+        let filteredData = data.filter(x => x[groupKey] == groupVal);
+        let countsDict = {}
+        for(let val of responseVals) {
+            countsDict[val] = 0;
+        }
+        
     }
 }
 
