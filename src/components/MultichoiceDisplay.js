430af9934bc6a059e74660e7f99eae512181eda1
Kevin Bi <keveloper@gmail.com>
2020-03-08 01:30:21 -0500

    Update some administrative stuff

--- src/components/MultichoiceDisplay.js
+++ src/components/MultichoiceDisplay.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Utils from '../Utils'
 
 export default class MultichoiceDisplay extends React.Component {
     render() { 
-        
+        let groupVals = Utils.getUniqueDictVals(this.props.data, this.props.group.id, Utils.getGroupSorter(this.props.group))
     }
 }
