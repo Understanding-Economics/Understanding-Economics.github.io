34c984cedf9f1f56cdc89a121ac72e5a2b13df1c
Kevin Bi <keveloper@gmail.com>
2020-04-06 16:00:20 -0500

    Change font size of charts and make "All" to be formatted differently

--- src/components/StatBubble.js
+++ src/components/StatBubble.js
@@ -3,10 +3,11 @@ import '../css/StatBubble.css'
 
 export default class StatBubble extends React.PureComponent { 
     render() { 
+    let style = this.props.title == "All" ? { textDecoration : "underline" } : {};
     let classNames = "StatBubble text-center card h-100 " + (this.props.active ? "active" : "");
         return (
-            <a href="#" className = {classNames} onClick = { (e) => {e.preventDefault(); this.props.handleClick()} }>
-                <strong className = "BubbleTitle">{this.props.title}</strong>
+            <a href="#" className = {classNames} onClick = { (e) => {e.preventDefault(); this.props.handleClick()}}>
+                <strong className = "BubbleTitle" style = {style}>{this.props.title == "All" ? this.props.title.toUpperCase() : this.props.title}</strong>
                 {
                     this.props.stat ? 
                     (this.props.formatter ? this.props.formatter(this.props.stat.toFixed(0)) : this.props.stat.toFixed(0)) :
