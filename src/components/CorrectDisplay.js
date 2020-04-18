53e48af9aeb6e3a8d8e3eb3b9cc8f3ce0014fa6f
Kevin Bi <keveloper@gmail.com>
2020-04-18 16:08:59 -0500

    fix #73 added source_url field to questions

--- src/components/CorrectDisplay.js
+++ src/components/CorrectDisplay.js
@@ -6,9 +6,20 @@ export default class CorrectDisplay extends React.PureComponent {
             return null;
         }
         let formatter =  this.props.formatter || (x => x);
+        let sourceText = null; 
+        if(this.props.question.source) {
+            if (this.props.question.source_url) {
+                sourceText = <span>    (Source: <a href = {this.props.question.source_url} target = "_blank">{this.props.question.source}</a>)</span>
+            }
+            else{
+                sourceText = <span>    (Source: {this.props.question.source})</span>
+            }
+        }
         return <div className = "header">
             <strong style={{color:"green", fontSize:"14pt"}}>Correct Answer: {formatter(this.props.question.correct)}</strong>
-            <em style={{fontSize:"8pt", marginLeft: "5px", verticalAlign: "top"}}>{this.props.question.source ? `  (Source: ${this.props.question.source})` : null}</em>
+            <em style={{fontSize:"8pt", marginLeft: "5px", verticalAlign: "top"}}>
+                {sourceText ? sourceText : null}
+            </em>
         </div>
     }
 }
