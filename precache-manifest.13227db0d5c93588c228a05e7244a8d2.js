434745a2a2f454a088ab1acd8a7043d3232df15a
Kevin Bi <keveloper@gmail.com>
2020-04-25 15:05:27 -0500

    Updates

---
+++ precache-manifest.13227db0d5c93588c228a05e7244a8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b3653145d51a2012d8a4b68b1561a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06682097b142f0bc1639",
+    "url": "/static/css/main.58593900.chunk.css"
+  },
+  {
+    "revision": "674a135b35f2059ba2d2",
+    "url": "/static/js/2.47a61bb3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.47a61bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06682097b142f0bc1639",
+    "url": "/static/js/main.b0ed883e.chunk.js"
+  },
+  {
+    "revision": "c80d6cc2a3f4104bf5e7",
+    "url": "/static/js/runtime-main.6b4e7937.js"
+  }
+]);
