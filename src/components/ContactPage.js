a68e4649933e512d5dae9dd78d8440745a0f619c
Kevin Bi <keveloper@gmail.com>
2020-05-01 12:39:29 -0500

    Rename to 'Stefanie' Stantcheva

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -10,9 +10,9 @@ export default class ContactPage extends React.PureComponent {
 
             Any technical issues can be reported to: <a href = "mailto:kevinbi@college.harvard.edu">kevinbi@college.harvard.edu</a>. <br/><br/>
 
-            You can also reach Professor Stantcheva via email at <a href = "mailto:sstantcheva@fas.harvard.edu">sstantcheva@fas.harvard.edu</a>
+            You can also reach Stefanie Stantcheva via email at <a href = "mailto:sstantcheva@fas.harvard.edu">sstantcheva@fas.harvard.edu</a>
             <Subsection>Other research</Subsection>
-            If you are interested in seeing other research projects from Professor Stantcheva's Social Economics Lab, you can visit the <a href = "http://socialeconomicslab.org/">Social Economics Lab Website</a>!
+            If you are interested in seeing other research projects from Stefanie Stantcheva's Social Economics Lab, you can visit the <a href = "http://socialeconomicslab.org/">Social Economics Lab Website</a>!
         </div>)
     }
 }
