2aaf248a7534f4b73380a77660756059035da9c6
Kevin Bi <keveloper@gmail.com>
2020-05-02 16:03:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bb1e0853c80d5173ccdc99875d46eb9.js"
+  "/precache-manifest.8cf4540eb0a4cd9b5f94283c197d556f.js"
 );
 
 self.addEventListener('message', (event) => {
